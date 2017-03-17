77f33b4aa47b4ba6917b50a3074e70ab77596505
Roque Santos <roqsantos@tjba.jus.br>
2017-03-17 17:52:46 -0300

    adição de transformer

--- psy/src/main/java/com/solutishackaton2017/psy/Application.java
+++ psy/src/main/java/com/solutishackaton2017/psy/Application.java
@@ -1,5 +1,7 @@
 package com.solutishackaton2017.psy;
 
+import com.solutishackaton2017.psy.transformer.JsonTransformer;
+
 import spark.Spark;
 
 public class Application {
@@ -7,6 +9,6 @@ public class Application {
 	public static void main(String[] args) {
 		Spark.get("/", (request, response) -> {
 		    return "";
-		});
+		}, new JsonTransformer());
 	}
 }
---
+++ psy/src/main/java/com/solutishackaton2017/psy/transformer/JsonTransformer.java
@@ -0,0 +1,14 @@
+package com.solutishackaton2017.psy.transformer;
+
+import com.google.gson.Gson;
+
+import spark.ResponseTransformer;
+
+public class JsonTransformer implements ResponseTransformer {
+	private Gson gson = new Gson();
+
+    @Override
+    public String render(Object model) {
+        return gson.toJson(model);
+    }
+}
