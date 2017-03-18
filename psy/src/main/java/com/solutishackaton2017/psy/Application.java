8547747576219e5b7bedfb330f94b12a389ab260
Roque Santos <roqsantos@tjba.jus.br>
2017-03-18 05:29:55 -0300

    ajuste pra renderizar html

--- psy/src/main/java/com/solutishackaton2017/psy/Application.java
+++ psy/src/main/java/com/solutishackaton2017/psy/Application.java
@@ -1,22 +1,33 @@
 package com.solutishackaton2017.psy;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import spark.Spark;
 
 public class Application {
 
+	private static final String basePath = "C:\\Desenvolvimento\\IDE\\jee-neon\\workspace\\solutishackathon2017\\psy\\src\\main\\resources\\public\\";
+	
 	public static void main(String[] args) {
 		Spark.staticFiles.location("/public");
 		
-		Spark.get("/", (request, response) -> {
-	        response.type("text/html");
-	        
-			return "index.html";
-		});
+		Spark.get("/", (request, response) -> renderContent("index.html"));
 		
-		Spark.get("/login", (request, response) -> {
-	        response.type("text/html");
-	        
-			return "login.html";
-		});
+		Spark.get("/login", (request, response) -> renderContent("login.html"));
+	}
+	
+	private static String renderContent(String htmlFile) {
+	    try {
+
+	        Path path = Paths.get(basePath + htmlFile);
+	        return new String(Files.readAllBytes(path), Charset.defaultCharset());
+	    } catch (IOException e) {
+	        // Add your own exception handlers here.
+	    }
+	    return null;
 	}
 }
